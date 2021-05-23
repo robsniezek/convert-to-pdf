1c3da762fb2a2a3ced7fa507decdb6fcd2d8765c
robsniezek <robertsniezek@gmail.com>
2021-05-23 19:21:38 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+
+const files = glob.sync(
+	'C:/Users/rober/Downloads/Firmex 21-05-23 172504/Due Diligence/TowerTrack/**/Documents/*'
+);
+console.log(files);
+
+files.forEach(file => {
+	console.log('from: ', file);
+	const changedDirectory = file.replace('original-directory', 'pdfs');
+
+	if (!path.extname(file)) {
+		changedFilename = changedDirectory.replace(
+			path.basename(file),
+			path.basename(file) + '.pdf'
+		);
+	} else {
+		changedFilename = file;
+	}
+
+	console.log('to: ', changedFilename);
+	fs.renameSync(file, changedFilename);
+});
