d938d3df4c30f69336bc9f92136b88d52c3c8041
robsniezek <robertsniezek@gmail.com>
2021-05-23 19:36:29 -0400

    cleaned Directory reerences

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 const path = require('path');
 
 const files = glob.sync(
-	'C:/Users/rober/Downloads/Firmex 21-05-23 172504/Due Diligence/TowerTrack/**/Documents/*'
+	// UPDATE DIRECTORY LOCATION
+	'./TowerTrack/**/Documents/*'
+	// './TowerTrack/Test/Documents/*'
+
+	// 'C:/Users/rober/Downloads/Firmex 21-05-23 172504/Due Diligence/TowerTrack/**/Documents/*'
 );
-console.log(files);
+// console.log(files);
 
 files.forEach(file => {
 	console.log('from: ', file);
+	// const changedDirectory = file;
 	const changedDirectory = file.replace('original-directory', 'pdfs');
 
 	if (!path.extname(file)) {
